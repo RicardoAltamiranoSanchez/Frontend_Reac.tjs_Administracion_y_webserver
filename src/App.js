bd33fde2ab594380db35cd36c2f4c26960a972f9
Ricardo <altamiranoricardo545@gmail.com>
2021-06-27 22:19:17 -0500

    Hicimos cambios en post aun no lo resuelvo segun son por los parametros y añadimos una nueva plantilla para informacion de usuario por individual y la recarga de la pagina

--- src/App.js
+++ src/App.js
@@ -1,102 +1,134 @@
 //para consumir las apis es con useEffect y useState
-import React ,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 //importamos la libreria para el envio de rutas o los paths
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Practica from './components/prueba';
 import PrimeraPagina from './components/primeraPagina';
 import CategoriasPagina from './components/segundaPagina';
+import Usuarioinfo from './components/Usuario';
 
 import ObtenerProducto from './components/producto';
 
 import Axios from './config/axios';
 
 
+
 function App() {
-  console.log(process.env.REACT_APP_BACKEND_URL);
-   const [Usuarios,guardaUsuarios]=useState([]);
-   const [Categorias,guardarCategorias]=useState([]);
-   const [Productos,guardarProductos]=useState([]);
-   //useEffect es un funcion que se ejecuta cuando hay cambios en el programa 
-  useEffect(()=>{
-     //consultamos la url ya no ponemos el local host por que ya la tenemos en una varible global
-  const consultarApi= ()=>{
-   
-     Axios.get('http://localhost:8080/Api/Usuarios')
-    .then(respuesta => {
-      guardaUsuarios(respuesta.data);
-    })
-    .catch(err => console.log(err)) 
-  }
-  //Consultamos en la ruta categoria de nuestra api
-  const ConsultarCategorias=()=>{
-    
-    Axios.get('http://localhost:8080/Api/categorias')
-    .then((response) => 
-    { 
-     
-      guardarCategorias(response.data); 
-    
-    })
-    .catch(err => console.log(err));
-
-  }
-  const ConsultarProductos=()=>{
-    Axios.get('http://localhost:8080/Api/productos')
-    .then((response) => 
-    
-    guardarProductos(response.data)
-    )
-    .catch(err => console.log(err))
-  }
-  ConsultarProductos();
-  ConsultarCategorias();
-  consultarApi();     
-  },[]);
- 
-  return (
-  <Router>
-   <Switch>
-     <Route
-     //utilizamos route para hacer el link o url de nuestro programas debemos crear los componentes de cada pagina que vamos a llamar
-      //Asi se pasa la informacion que estemos consumiendo una api se pasa cuando quieras pasar useState o funcion se debe usar esta sintaxis
-        exact path='/'
-        component={()=> <Practica 
-        Usuarios={Usuarios}  
-        />}
-
-     />
-
-     <Route
-     exact path='/CrearUsuario'
-     component={PrimeraPagina}
-     />
-     
-     <Route
-     exact path='/Categorias'
-     component={()=> <CategoriasPagina
-     Categorias={Categorias}
-     
-     />}
-     />
-     <Route  
-    exact path='/Productos'
-    component={ ()=> <ObtenerProducto
-    Productos={Productos}
-    
+    console.log(process.env.REACT_APP_BACKEND_URL);
+    const [Usuarios, guardaUsuarios] = useState([]);
+    const [Categorias, guardarCategorias] = useState([]);
+    const [Productos, guardarProductos] = useState([]);
+    const [consulta, guardarConsulta] = useState(true);
+    //useEffect es un funcion que se ejecuta cuando hay cambios en el programa 
+    useEffect(() => {
+        //Creamos un useState para que se recargue la pagina cada vez que insertamos algo 
+        if (consulta) {
+            //consultamos la url ya no ponemos el local host por que ya la tenemos en una varible global
+            const consultarApi = () => {
+                Axios.get('http://localhost:8080/Api/Usuarios')
+                    .then(respuesta => {
+                        guardaUsuarios(respuesta.data);
+                        // lo cambiamos en falso para no que no recargue la pagina
+                        guardarConsulta(false);
+                    })
+                    .catch(err => console.log(err))
+            }
 
 
-    />}
-    />
-   </Switch>
+            //Consultamos en la ruta categoria de nuestra api
+            const ConsultarCategorias = () => {
+
+                Axios.get('http://localhost:8080/Api/categorias')
+                    .then((response) => {
+
+                        guardarCategorias(response.data);
+
+                    })
+                    .catch(err => console.log(err));
+
+            }
+            const ConsultarProductos = () => {
+                Axios.get('http://localhost:8080/Api/productos')
+                    .then((response) =>
+
+                        guardarProductos(response.data)
+                    )
+                    .catch(err => console.log(err))
+            }
+
+            ConsultarProductos();
+            ConsultarCategorias();
+            consultarApi();
+        }
+    }, [consulta]);
+    //de eciamos en consulata en corchetes que cuando ese cambie este atento y vuelva a ejecutar ese codigo para eso es el
+
+    return ( <
+            Router >
+            <
+            Switch >
+            <
+            Route
+            //utilizamos route para hacer el link o url de nuestro programas debemos crear los componentes de cada pagina que vamos a llamar
+            //Asi se pasa la informacion que estemos consumiendo una api se pasa cuando quieras pasar useState o funcion se debe usar esta sintaxis
+            exact path = '/'
+            component = {
+                () => < Practica
+                Usuarios = { Usuarios }
+                />}
+
+                /
+                >
+
+                <
+                Route
+                exact path = '/CrearUsuario'
+                component = {
+                    () => < PrimeraPagina
+                    guardarConsulta = { guardarConsulta }
+                    />} /
+                    >
+
+                    <
+                    Route
+                    exact path = '/Categorias'
+                    component = {
+                        () => < CategoriasPagina
+                        Categorias = { Categorias }
+
+                        />} /
+                        >
+                        <
+                        Route
+                        exact path = '/Productos'
+                        component = {
+                            () => < ObtenerProducto
+                            Productos = { Productos }
 
 
-  </Router>
+
+                            />}
+
+                            /
+                            >
+                            <
+                            Route
+                            exact path = '/Usuario/:id'
+                            component = { Usuarioinfo }
+
+
+                            /> <
+                            /Switch>
+
+
+                            <
+                            /Router>
 
 
 
-  );
-}
+                        );
+                    }
 
-export default App;
-//debemos instalar axios para hacer las peticiones ala api y obtener la informacion de la base de datos 
-//npm install axios
+                    export default App;
+                    //debemos instalar axios para hacer las peticiones ala api y obtener la informacion de la base de datos 
+                    //npm install axios
--- src/components/primeraPagina.js
+++ src/components/primeraPagina.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+//para recuperar los props despues que se recargue la pagina es withRouter
 import Axios from '../config/axios'
 
 //debemos poner siempre con mayusculas la primera letra en una funcion en react marcar mucho errores por eso
@@ -8,8 +9,6 @@ const PrimeraPagina = (props) => {
     const [NuevoUsuario, GuardarNuevoUsuario] = useState({
         nombre: '',
         correo: '',
-        telefono: '',
-        hora: '',
         password: '',
         descripcion: '',
         rol: ''
@@ -25,129 +24,151 @@ const PrimeraPagina = (props) => {
             [e.target.name]: [e.target.value]
 
         })
+
     }
     const CrearNuevoUsuario = (e) => {
         e.preventDefault();
 
         Axios.post('http://localhost:8080/Api/Usuarios', NuevoUsuario)
-            .then( response => {
+            .then(response => {
 
                 console.log(response.data);
+                console.log(NuevoUsuario);
+                //lo ponemos en verdadero para refrescar la pagina
+                props.guardarConsulta = (true);
+
                 // Redireccionar
                 props.history.push('/')
             })
     }
 
-    return (
-        <Fragment>
-            <h1>Crear Usuario</h1>
-            <div className="container mt-5 py-5">
-                <div className="row">
+    return ( <
+        Fragment >
+        <
+        h1 > Crear Usuario < /h1> <
+        div className = "container mt-5 py-5" >
+        <
+        div className = "row" >
 
-                    <div className="col-12 mb-5 d-flex justify-content-center">
-                        <Link to={'/'} className="btn btn-success text-uppercase py-2 px-5 font-weight-bold"> Regresar</Link>
-                    </div>
-                    <div className="col-md-8 mx-auto">
-                        <form onSubmit={CrearNuevoUsuario} className="bg-white p-5 bordered">
+        <
+        div className = "col-12 mb-5 d-flex justify-content-center" >
+        <
+        Link to = { '/' }
+        className = "btn btn-success text-uppercase py-2 px-5 font-weight-bold" > Regresar < /Link> <
+        /div> <
+        div className = "col-md-8 mx-auto" >
+        <
+        form onSubmit = { CrearNuevoUsuario }
+        className = "bg-white p-5 bordered" >
 
-                            <div className="form-group">
-                                <label htmlFor="nombre">Nombre Completo </label>
-                                <input
-                                    type="text"
-                                    className="form-control form-control-lg"
-                                    id="nombre"
-                                    name="nombre"
-                                    placeholder="Nombre Completo Usuario"
-                                    //Es una funcion de boton de react con onchange y metemos la funcion
-                                    onChange={AgregarNuevoUsuario}
-                                />
-                            </div>
-                            <div className="from-group">
-                                <label htmlFor='correo'>Correo</label>
-                                <input
-                                type="email"
-                                className="form-control form-control-"
-                                id='correo'
-                                name='correo'
-                                onChange={AgregarNuevoUsuario}
-                           />
-                            </div>
+        <
+        div className = "form-group" >
+        <
+        label htmlFor = "nombre" > Nombre Completo < /label> <
+        input type = "text"
+        className = "form-control form-control-lg"
+        id = "nombre"
+        name = "nombre"
+        placeholder = "Nombre Completo Usuario"
+        //Es una funcion de boton de react con onchange y metemos la funcion
+        onChange = { AgregarNuevoUsuario }
+        /> <
+        /div> <
+        div className = "from-group" >
+        <
+        label htmlFor = 'correo' > Correo < /label> <
+        input type = "email"
+        className = "form-control form-control-"
+        id = 'correo'
+        name = 'correo'
+        onChange = { AgregarNuevoUsuario }
+        /> <
+        /div>
 
-                            <div className="form-group">
-                                <label htmlFor="propietario">Rol</label>
-                                <input
-                                    type="text"
-                                    className="form-control form-control-lg"
-                                    id="rol"
-                                    name="rol"
-                                    placeholder="Rol Usuario"
-                                    onChange={AgregarNuevoUsuario}
-                                />
-                            </div>
+        <
+        div className = "form-group" >
+        <
+        label htmlFor = "propietario" > Rol < /label> <
+        input type = "text"
+        className = "form-control form-control-lg"
+        id = "rol"
+        name = "rol"
+        placeholder = "Rol Usuario"
+        onChange = { AgregarNuevoUsuario }
+        /> <
+        /div>
 
-                            <div className="form-group">
-                                <label htmlFor="telefono">Teléfono</label>
-                                <input
-                                    type="tel"
-                                    className="form-control form-control-lg"
-                                    id="telefono"
-                                    name="telefono"
-                                    placeholder="Teléfono"
-                                    onChange={AgregarNuevoUsuario}
-                                />
-                            </div>
+        <
+        div className = "form-group" >
+        <
+        label htmlFor = "telefono" > Teléfono < /label> <
+        input type = "tel"
+        className = "form-control form-control-lg"
+        id = "telefono"
+        name = "telefono"
+        placeholder = "Teléfono"
+        onChange = { AgregarNuevoUsuario }
+        /> <
+        /div>
 
-                            <div className="form-group">
-                                <label htmlFor="fecha">Fecha Alta</label>
-                                <input
-                                    type="date"
-                                    className="form-control form-control-lg"
-                                    id="fecha"
-                                    name="fecha"
-                                    onChange={AgregarNuevoUsuario}
-                                />
-                            </div>
-
-                           
-                            <div className="form-group">
-                                <label htmlFor="hora">Hora Alta</label>
-                                <input
-                                    type="time"
-                                    className="form-control form-control-lg"
-                                    id="hora"
-                                    name="hora"
-                                    onChange={AgregarNuevoUsuario}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Contraseña</label>
-                                <input
-                                type="password"
-                                className="form-control"
-                                id='password'
-                                name="password"
-                                onChange={AgregarNuevoUsuario}
-                                />
-                            </div>
+        <
+        div className = "form-group" >
+        <
+        label htmlFor = "fecha" > Fecha Alta < /label> <
+        input type = "date"
+        className = "form-control form-control-lg"
+        id = "fecha"
+        name = "fecha"
+        onChange = { AgregarNuevoUsuario }
+        /> <
+        /div>
 
 
-                            <div className="form-group">
-                                <label htmlFor="descripcion">Descripcion</label>
-                                <textarea
-                                    className="form-control"
-                                    name="descripcion"
-                                    rows="6"
+        <
+        div className = "form-group" >
+        <
+        label htmlFor = "hora" > Hora Alta < /label> <
+        input type = "time"
+        className = "form-control form-control-lg"
+        id = "hora"
+        name = "hora"
+        onChange = { AgregarNuevoUsuario }
+        /> <
+        /div> <
+        div className = "form-group" >
+        <
+        label htmlFor = "password" > Contraseña < /label> <
+        input type = "password"
+        className = "form-control"
+        id = 'password'
+        name = "password"
+        onChange = { AgregarNuevoUsuario }
+        /> <
+        /div>
 
-                                    onChange={AgregarNuevoUsuario}
-                                ></textarea>
-                            </div>
-                            <input type="submit" className="btn btn-primary mt-3 w-100 p-3 text-uppercase font-weight-bold" value="Crear Cuenta" />
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </Fragment>
+
+        <
+        div className = "form-group" >
+        <
+        label htmlFor = "descripcion" > Descripcion < /label> <
+        textarea className = "form-control"
+        name = "descripcion"
+        rows = "3"
+
+        onChange = { AgregarNuevoUsuario } >
+        < /textarea> <
+        /div> <
+        input type = "submit"
+        className = "btn btn-primary mt-3 w-100 p-3 text-uppercase font-weight-bold"
+        value = "Crear Cuenta" / >
+        <
+        /form> <
+        /div> <
+        /div> <
+        /div> <
+        /Fragment>
     );
 }
 
-export default PrimeraPagina;
+export default withRouter(PrimeraPagina);
+//lo rodeamo para recuperar los props
